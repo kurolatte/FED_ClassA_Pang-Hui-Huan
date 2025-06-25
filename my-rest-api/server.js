993f4a27c4e27ce8ce846e2c3047478e241cbc5a
kurolatte <panghuihuan@gmail.com>
2025-06-26 00:19:03 +0800

    rearrange files

--- my-rest-api/server.js
+++ my-rest-api/server.js
@@ -5,21 +5,23 @@ const path       = require('path');
 const data       = require('./data/data.js');
 
 const app = express();
-
 app.use(bodyParser.json());
 app.use(cors());
-
-app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/api/products', (req, res) => {
   res.json(data.products);
 });
-
 app.get('/api/products/:id', (req, res) => {
   const id = +req.params.id;
   const product = data.products.find(p => p.id === id);
   if (!product) return res.status(404).json({ error: 'Not found' });
   res.json(product);
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'products.html'));
 });
 
 const port = process.env.PORT || 3000;
